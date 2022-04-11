b8f8528540895748b17dfe91898cd3773f856455
jeonghaa <93295701+jeonghaa@users.noreply.github.com>
2022-04-11 11:23:00 +0900

    Add files via upload

--- js/donut.js
+++ js/donut.js
@@ -8,45 +8,6 @@ $(document).ready(function() {
             $('header').removeClass("fixed"); 
         } 
     });
-    //
-
-
-    //메인 슬라이드
-   $('.mslider').lightSlider({
-        adaptiveHeight:true,
-        item:1,
-        auto: true,
-        speed: 500,
-        pause: 5000,
-        pauseOnHover: true,
-        slideMargin:0,
-       controls: true,
-        loop:true
-    });
-
-    //탭메뉴
-    $('.tab_go').click(function(){
-        $(this).parent().addClass('current')
-        .siblings().removeClass('current');
-        return false;
-    });
-    
-
-
-    //sns사진 부분
-    $('.sns_pic').lightSlider({
-        adaptiveHeight:true,
-        item:4,
-        auto: true,
-        speed: 300,
-        pause: 2000,
-        pauseOnHover: true,
-        slideMargin:4,
-        controls: false,
-        pager: false,
-        loop:true
-    });
-
 
     //top버튼
     $(document).ready(function(){
@@ -63,5 +24,13 @@ $(document).ready(function() {
         event.preventDefault();
         $('html, body').animate({scrollTop: 0}, 500);
     });
+        
+    //탭메뉴
+    $('.tab_go').click(function(){
+        $(this).parent().addClass('current')
+        .siblings().removeClass('current');
+        return false;
+    });
+    
 });
 
